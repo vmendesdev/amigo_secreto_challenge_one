5e9c01749312767487db2eb995677635710eb7f1
vmendesdev <vmendesjob@gmail.com>
2025-01-30 09:13:06 -0300

    feat:Implementa uma função para sortear amigos

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-let amigos = [];
+let listaDeAmigos = [];
 
 //Implementa uma função para agregar amigos
 function adicionarAmigo() {
-    let amigo= document.getElementById('amigo').value.trim();
+    let amigo = document.getElementById('amigo').value.trim();
     
-    if (amigo === '') {
+    if (amigo === "") {
         alert('Por favor, insira um nome válido.');
         return;
     }
-    
     listaDeAmigos.push(amigo);
-    document.getElementById('amigo').value = '';
+    document.getElementById('amigo').value = "";
     atualizarLista();
 }
 
@@ -24,4 +23,20 @@ function atualizarLista() {
         li.textContent = amigo;
         listaAmigosElement.appendChild(li);
     });
+}
+
+
+
+
+//Implementa uma função para sortear amigos
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        alert('Adicione pelo menos um nome à lista.');
+        return;
+    }
+        
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length); // Usando let para indiceAleatorio
+    let amigoSorteado = listaDeAmigos[indiceAleatorio]; // Usando let para amigoSorteado
+        
+    document.getElementById('resultado').textContent = `O amigo secreto é: ${amigoSorteado}`;
 }
