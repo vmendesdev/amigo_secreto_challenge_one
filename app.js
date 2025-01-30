826e48a325a061b40296df5114b0e3815de0b909
vmendesdev <vmendesjob@gmail.com>
2025-01-30 08:38:47 -0300

    feat:Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+//Implementa uma função para agregar amigos
+function adicionarAmigo() {
+    let amigo= document.getElementById('amigo').value.trim();
+    
+    if (amigo === '') {
+        alert('Por favor, insira um nome válido.');
+        return;
+    }
+    
+    listaDeAmigos.push(amigo);
+    document.getElementById('amigo').value = '';
+    atualizarLista();
+}
